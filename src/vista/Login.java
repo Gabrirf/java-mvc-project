304e21a9559728f2f1493bc4321b6728030a3bc7
Gabri <gabrodflo@us.es.com>
2021-06-02 17:23:33 +0200

    añadir botón a la ventana

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -1,11 +1,9 @@
 package vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
@@ -16,10 +14,13 @@ public class Login extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Boton");
+		btnNewButton.setBounds(169, 166, 89, 23);
+		contentPane.add(btnNewButton);
 		
 		setVisible(true);
 	}
-
 }
