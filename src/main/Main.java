13dea5a0ec9f193ba29566d728a66ffc01cad798
Gabri <gabrodflo@us.es.com>
2021-06-07 17:15:31 +0200

    Crear diseño de la vista tabla alumnos

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,12 +2,14 @@ package main;
 
 import bbdd.Conexion;
 import vista.Login;
+import vista.TablaAlumnos;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Conexion.conectar();
-		new Login();
+		//new Login();
+		new TablaAlumnos();
 	}
 
 }
--- src/vista/TablaAlumnos.java
+++ src/vista/TablaAlumnos.java
@@ -3,20 +3,42 @@ package vista;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JButton;
 
 public class TablaAlumnos extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	public TablaAlumnos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		table = new JTable();
+		table.setBounds(10, 11, 573, 339);
+		contentPane.add(table);
+		
+		JButton botonCrear = new JButton("Crear");
+		botonCrear.setBounds(593, 11, 112, 56);
+		contentPane.add(botonCrear);
+		
+		JButton botonBorrar = new JButton("Borrar");
+		botonBorrar.setBounds(593, 78, 112, 56);
+		contentPane.add(botonBorrar);
+		
+		JButton botonModificar = new JButton("Modificar");
+		botonModificar.setBounds(593, 145, 112, 56);
+		contentPane.add(botonModificar);
+		
+		JButton botonRefrescar = new JButton("Refrescar");
+		botonRefrescar.setBounds(593, 294, 112, 56);
+		contentPane.add(botonRefrescar);
+		
 		setVisible(true);
 	}
-
 }
