ebfc910857d8397e48aca428591cffb06d63d1c5
Gabri <gabrodflo@us.es.com>
2021-06-09 16:06:29 +0200

    Cambio para no cerrar el programa cuando cierro el formulario

--- src/vista/FormularioAlumno.java
+++ src/vista/FormularioAlumno.java
@@ -23,7 +23,7 @@ public class FormularioAlumno extends JFrame {
 	private JTextField textoAnio;
 
 	public FormularioAlumno() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
