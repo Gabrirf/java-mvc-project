404384103b5086d9c2b76d8156b42e4825162449
Gabri <gabrodflo@us.es.com>
2021-06-09 16:32:52 +0200

    Montar tabla en la pantalla principal con datos de prueba

--- src/controlador/Central.java
+++ src/controlador/Central.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 import beans.Alumno;
 import modelo.Alumnos;
@@ -18,6 +19,15 @@ public class Central {
 		Alumno alumno = new Alumno(-1, nombre, fechaNacimiento);
 		new Alumnos().crearAlumno(alumno);
 		
+	}
+	
+	public ArrayList<Alumno> recogerTodosAlumnos(){
+		// TODO: Llamar al modelo para recoger los alumnos
+		ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+		alumnos.add(new Alumno(1,"Alfonso", new Date(0)));
+		alumnos.add(new Alumno(2,"Rodolfo", new Date(10)));
+		alumnos.add(new Alumno(3,"Antoñito", new Date(20)));
+		return alumnos;
 	}
 
 }
